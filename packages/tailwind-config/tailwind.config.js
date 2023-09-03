c9ef71dfa61a2de7ea90b02c0cc5b3c5fdf59448
Luke Macauley <lukemacauley@mac.com>
2023-09-03 20:35:10 +0400

    change tailwind config

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
-    "./pages/**/*.{js,ts,jsx,tsx}",
+    // "./pages/**/*.{js,ts,jsx,tsx}",
     "./ui/**/*.{js,ts,jsx,tsx}",
-    // `src/**/*.{js,ts,jsx,tsx}`,
+    `src/**/*.{js,ts,jsx,tsx}`,
     "../../packages/core/src/*.{js,ts,jsx,tsx}",
   ],
   theme: {},
