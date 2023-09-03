64dbfa428da6b2e0bfa180904205858375222d83
Luke Macauley <lukemacauley@mac.com>
2023-09-03 16:36:29 +0400

    adjust tailwind config

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class", ".dark-theme"],
   content: [
     "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./ui/**/*.{js,ts,jsx,tsx}",
-    `src/**/*.{js,ts,jsx,tsx}`,
+    // `src/**/*.{js,ts,jsx,tsx}`,
     "../../packages/core/src/*.{js,ts,jsx,tsx}",
   ],
   theme: {},
